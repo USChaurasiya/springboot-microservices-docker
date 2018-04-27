689a726e78ce6e07d9d91531b1b5e845f474a9d0
Uma Shankar <uma.chaurasiya@bridgelabz.com>
2018-04-27 11:51:36 +0530

    Services Module added

---
+++ notification-service/src/main/java/com/uma/notificationservice/NotificationServiceApplication.java
@@ -0,0 +1,12 @@
+package com.uma.notificationservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NotificationServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NotificationServiceApplication.class, args);
+	}
+}
